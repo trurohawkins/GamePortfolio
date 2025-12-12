8ab99b18c701b13282d5f1376ee1477eac483e44
Fudge Breadstein <trubhawkins@gmail.com>
2025-12-12 12:40:24 -0500

    fixing file paths

--- src/preload.ts
+++ src/preload.ts
@@ -17,11 +17,11 @@ export class PreloadScene extends Phaser.Scene {
 			frameWidth: 32,
 			frameHeight: 32
 		});
-		this.load.image('planet', '/assets/planet.png');
-		this.load.image('background', '/assets/mindBG.png');
-		this.load.image('space', '/assets/space.png');
-		this.load.image('pizza', '/assets/pizza.png');
-		this.load.image('cloud', '/assets/neuronCloud.png');
+		this.load.image('planet', 'assets/planet.png');
+		this.load.image('background', 'assets/mindBG.png');
+		this.load.image('space', 'assets/space.png');
+		this.load.image('pizza', 'assets/pizza.png');
+		this.load.image('cloud', 'assets/neuronCloud.png');
 		this.load.video('gloryDogs', 'assets/trailers/gloryDogsTrailer.mp4');//, 'loadeddata', true, true);
 		this.load.video('stroid', 'assets/trailers/StroidTrailer.mp4');
 		this.load.video('trembles', 'assets/trailers/tremblesTrailer.mp4');
@@ -29,19 +29,19 @@ export class PreloadScene extends Phaser.Scene {
 		this.load.video('ROHLB', 'assets/trailers/RollOnHomeLittleBuddy.mp4');
 
 		for (let i = 0; i < 5; i++) {
-			this.load.image('gd' + i, '/assets/gameScreenshots/gloryDogs'+i+'.png');
+			this.load.image('gd' + i, 'assets/gameScreenshots/gloryDogs'+i+'.png');
 		}
 		for (let i = 0; i < 6; i++) {
-			this.load.image('stroid' + i, '/assets/gameScreenshots/stroid'+i+'.png');
+			this.load.image('stroid' + i, 'assets/gameScreenshots/stroid'+i+'.png');
 		}
 		for (let i = 0; i < 6; i++) {
-			this.load.image('trembles' + i, '/assets/gameScreenshots/trembles'+i+'.png');
+			this.load.image('trembles' + i, 'assets/gameScreenshots/trembles'+i+'.png');
 		}
 		for (let i = 0; i < 4; i++) {
-			this.load.image('pp' + i, '/assets/gameScreenshots/pp'+i+'.png');
+			this.load.image('pp' + i, 'assets/gameScreenshots/pp'+i+'.png');
 		}
 		for (let i = 0; i < 5; i++) {
-			this.load.image('rohlb' + i, '/assets/gameScreenshots/rohlb'+i+'.png');
+			this.load.image('rohlb' + i, 'assets/gameScreenshots/rohlb'+i+'.png');
 		}
 	}
 
